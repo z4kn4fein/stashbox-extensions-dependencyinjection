59fff1159af9c0577fb5ad5bc740948b7e20fee4
Peter Csajtai <peter.csajtai@outlook.com>
2017-02-18 00:32:49 +0100

    Modified patcher script

--- patch-version.ps1
+++ patch-version.ps1
@@ -1,5 +1,7 @@
 param($projectPath, $version)
 
+Write-Host "Patching with version $version"
+
 $csprojPath = Join-Path $PSScriptRoot $projectPath
 [xml]$project = Get-Content -Path $csprojPath
 $project.Project.PropertyGroup.Version = $version
