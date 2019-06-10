2b36415be0979785a246018d911e16e545c72240
Peter Csajtai <peter.csajtai@outlook.com>
2019-06-10 23:40:04 +0200

    Fix patcher script

--- patch-version.ps1
+++ patch-version.ps1
@@ -4,5 +4,10 @@ Write-Host "Patching with version $version"
 
 $csprojPath = Join-Path $PSScriptRoot $projectPath
 [xml]$project = Get-Content -Path $csprojPath
-$project.Project.PropertyGroup[0].Version = $version
+$group = $project.Project.PropertyGroup;
+if ($group.count -gt 1) {
+    $group[0].Version = $version
+} else {
+    $group.Version = $version
+}
 $project.Save($csprojPath)
